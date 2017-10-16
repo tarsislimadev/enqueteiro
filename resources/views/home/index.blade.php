b7542399ca5af177a8993fd7ba714a21968b3ea6
User <User@User-PC>
2017-10-16 11:24:59 -0200

    Adicionando página principal

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.default')
+
+@section('content')
+<div class="jumbotron">
+    <h1>Criar enquetes</h1>
+    <p class="lead">Crie suas enquetes de maneira rápida e prática.</p>
+    <p><a class="btn btn-lg btn-success" href="#" role="button">Começe agora</a></p>
+</div>
+
+<div class="row marketing">
+    <div class="col-lg-6">
+        <h4>Subheading</h4>
+        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+        <h4>Subheading</h4>
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+        <h4>Subheading</h4>
+        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+    </div>
+
+    <div class="col-lg-6">
+        <h4>Subheading</h4>
+        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+        <h4>Subheading</h4>
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+        <h4>Subheading</h4>
+        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+    </div>
+</div>
+@endsection
