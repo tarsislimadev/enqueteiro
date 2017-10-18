337a6dcc726a79e274f5f1e550f4e8a64990bdfb
Tarsis <tarsis@meuposvenda.com.br>
2017-10-18 12:09:19 -0200

    Listando as últimas enquetes na página principal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 class HomeController extends Controller {
 
     public function index() {
-        return view('home.index');
+        $forms = \App\Form::query()->skip(0)->take(24)->orderBy('id', 'desc')->get();
+        
+        return view('home.index')->with(\compact('forms'));
     }
     
     public function create() {
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,26 +8,14 @@
 </div>
 
 <div class="row marketing">
+    @foreach($forms->chunk(3) as $line)
     <div class="col-lg-6">
-        <h4>Subheading</h4>
-        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+        @foreach($line as $form)
+        <h4>{{ $form['title'] }}</h4>
+        <p><a href="{{ route('iframe', ['id' => $form['id']]) }}">responder</a></p>
 
-        <h4>Subheading</h4>
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-        <h4>Subheading</h4>
-        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        @endforeach
     </div>
-
-    <div class="col-lg-6">
-        <h4>Subheading</h4>
-        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-        <h4>Subheading</h4>
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-        <h4>Subheading</h4>
-        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-    </div>
+    @endforeach
 </div>
 @endsection
