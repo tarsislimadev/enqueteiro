d515cb11f04696bbf718a0976df9a19ebbc7a687
Tarsis <tarsis@meuposvenda.com.br>
2017-10-18 10:37:36 -0200

    Adicionando página Sobre

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,5 +54,9 @@ class HomeController extends Controller {
         
         return view('home.answered');
     }
+    
+    public function about() {
+        return view('home.about');
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ Route::get('create', ['as' => 'create', 'uses' => 'HomeController@create']);
 Route::post('save', ['as' => 'save', 'uses' => 'HomeController@save']);
 Route::get('iframe', ['as' => 'iframe', 'uses' => 'HomeController@iframe']);
 Route::post('send/{id}', ['as' => 'send', 'uses' => 'HomeController@send']);
+
+Route::get('about', ['as' => 'about', 'uses' => 'HomeController@about']);
