89b21b73399061c442d22eaf231c782ba33ca27a
Tarsis de Lima <tarsis.mikael@hotmail.com>
2017-10-19 23:47:45 -0200

    Alterando lista de enquetes para listar enquetes criadas a partir de uma hora antes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers;
 class HomeController extends Controller {
 
     public function index() {
-        $forms = \App\Form::query()->skip(0)->take(12)->orderBy('id', 'desc')->get();
-        
+        $oneHourAgo = \Carbon\Carbon::now()->subHour();
+        $forms = \App\Form::query()
+            ->skip(0)
+            ->take(12)
+            ->where('updated_at', '<', $oneHourAgo)
+            ->orderBy('id', 'desc')
+            ->get();
+
         return view('home.index')->with(\compact('forms'));
     }
-    
+
     public function about() {
         return view('home.about');
     }
