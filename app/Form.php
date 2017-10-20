b16010c199d869cdc6a1891d96dd5e5015bddc42
Tarsis <tarsis@meuposvenda.com.br>
2017-10-20 10:21:20 -0200

    Relacionando modelo de Enquete com modelo de Resposta

--- app/Form.php
+++ app/Form.php
@@ -9,4 +9,9 @@ class Form extends Model
     protected $fillable = [
         'title', 'options', 'hash', 'owner',
     ];
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class, 'form_id', 'id');
+    }
 }
