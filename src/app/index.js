b0815408f307bb51721bbf30b2dbb503c8cfca12
Tarsis <br.tmvdl@gmail.com>
2023-12-28 16:22:41 -0300

    Updating app files

---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import { Server } from '@brtmvdl/backend'
+
+const server = new Server()
+
+server.get('/', (_, res) => res.setJSON({}))
+
+server.post('/', (_, res) => res.setJSON({}))
+
+server.listen(80)
