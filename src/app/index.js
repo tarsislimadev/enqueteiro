538307b57af1db632e448da143ff0ef081070078
Tarsis Lima <br.tmvdl@gmail.com>
2024-12-09 15:27:10 +0000

    Updating app files

--- src/app/index.js
+++
@@ -1,7 +0,0 @@
-import { Server } from '@brtmvdl/backend'
-
-const server = new Server()
-
-server.get('/', (_, res) => res.setView('./views/index.html', { name: 'enqueteiro' }))
-
-server.listen('80')
